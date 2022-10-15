af096aabdfc3c66e10e928bd055a77a543606a62
Aakash Rikh <aakashrikh@Aakash-MacBook-Pro.local>
2022-10-15 14:56:52 +0530

    changes

--- Screens/Home.js
+++ Screens/Home.js
@@ -57,9 +57,7 @@ class Home extends Component {
 
   componentDidMount= ()=> {
 
-    window.Echo.private(`checkTableStatus.1`).listen('.server.created', (e) => {
-      alert(e.id);
-  });
+  
 
 
   //   // alert(global.vendor)
